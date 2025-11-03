53f0cd4fab4808a9bbdd147f44b5e87229896a79
Sara Lorena Duque Ramirez <sara.duque1@udea.edu.co>
2025-11-03 15:31:25 -0500

    Agregando ruta de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TestConnection } from '@/components/TestConnection';
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -29,6 +30,9 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Navigate to="/login" replace />} />
               <Route path="/login" element={<Login />} />
+              
+              <Route path="/test" element={<TestConnection />} />
+              
               <Route path="/panel" element={
                 <ProtectedRoute>
                   <Panel />
